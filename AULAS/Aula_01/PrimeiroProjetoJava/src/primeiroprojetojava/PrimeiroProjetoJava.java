1f82b66bcfa776189bae275cbde21177e9762fb5
Samuel Seven <>
2024-05-21 10:27:40 -0300

    feat: adicionado hello world

--- AULAS/Aula_01/PrimeiroProjetoJava/src/primeiroprojetojava/PrimeiroProjetoJava.java
+++ AULAS/Aula_01/PrimeiroProjetoJava/src/primeiroprojetojava/PrimeiroProjetoJava.java
@@ -15,7 +15,9 @@ public class PrimeiroProjetoJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");
+        
+        // Este e meu primeiro projeto utilizando JAVA
     }
     
 }
